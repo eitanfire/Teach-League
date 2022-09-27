a43d146c2540db4a815d7815808de625b1336d98
Eitan Fire <eitanfire@gmail.com>
2022-09-26 21:58:11 -0600

    add h2 text and restyle header

--- teach-league/src/pages/TheTeachersLounge.js
+++ teach-league/src/pages/TheTeachersLounge.js
@@ -1,8 +1,22 @@
 import { Col, Row, Container } from 'reactstrap';
+import { Card, CardBody } from 'reactstrap';
 
 const TheTeachersLounge = () => {
     return (
-        <h1>The Teachers Lounge</h1>
+        <Container>
+            <Card>
+           <CardBody>
+            <Row>
+                <Col>
+        { <h1>The Teachers Lounge</h1> }
+                </Col>
+                <Col>
+        { <h2>Know that dream where you show up for the final and realize you forgot to go to class? Yeah we have that too but were teaching the class.</h2> }
+                </Col>
+        </Row>
+        </CardBody>
+        </Card>
+        </Container>
     );
 }
 
