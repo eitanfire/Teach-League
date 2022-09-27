ccb3a6f3cf155310b142a7e590475d89e53f876d
Eitan Fire <eitanfire@gmail.com>
2022-09-26 21:38:05 -0600

    TheTeachersLounge

--- teach-league/src/App.js
+++ teach-league/src/App.js
@@ -3,13 +3,14 @@ import { Container, Col, Row } from 'reactstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import React from 'react';
-
+import TheTeachersLounge from './pages/TheTeachersLounge';
 
 function App() {
   return (
     <div className="App">
   <Container>
     <Header className='col col-12' />
+    <TheTeachersLounge />
       {/* <h1>
         Hello World!
       </h1> */}
---
+++ teach-league/src/pages/TheTeachersLounge.js
@@ -0,0 +1,9 @@
+import { Col, Row, Container } from 'reactstrap';
+
+const TheTeachersLounge = () => {
+    return (
+        <h1>The Teachers Lounge</h1>
+    );
+}
+
+export default TheTeachersLounge
