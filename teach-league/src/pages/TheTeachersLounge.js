487ac511746e9fc46a2b6d487701a0d591dfdb47
Eitan Fire <eitanfire@gmail.com>
2022-09-30 20:55:02 -0600

    Refactored LoungeCardList to TheTeachersLounge page

--- teach-league/src/pages/TheTeachersLounge.js
+++ teach-league/src/pages/TheTeachersLounge.js
@@ -2,6 +2,8 @@ import { Col, Row, Container } from 'reactstrap';
 import { Card, CardBody } from 'reactstrap';
 import SecretTeacher from '../features/SecretTeacher.js';
 import LoungeCard from '../components/LoungeCard';
+import LoungeCardList from '../utils/LoungeCardList';
+import { accordionData } from '../utils/teachersLoungeContent.js';
 
 const TheTeachersLounge = () => {
     return (
@@ -14,6 +16,13 @@ const TheTeachersLounge = () => {
                 </Col>
                 <Col col col-md-8>
         { <h2>Know that dream where you show up for the final and realize you forgot to go to class? Yeah we have that too but were supposed to be teaching it!</h2> }
+                </Col>
+        </Row>
+        <Row>
+        <Col className="accordion col-12 col-lg-9 ">
+                    {accordionData.map(({ title, content, image }) => (
+                        <LoungeCardList title={title} content={content} image={image} />
+                    ))}
                 </Col>
         </Row>
         <LoungeCard />
