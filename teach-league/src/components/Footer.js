5d255874dc680f825e55414edb4b44c1131a3935
Eitan Fire <eitanfire@gmail.com>
2022-09-30 21:49:40 -0600

    Style teachersLoungeContent

--- teach-league/src/components/Footer.js
+++ teach-league/src/components/Footer.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Footer = () => {
     return (
-<p id='Footer'>© Teach League <span style={{color: 'grey'}}>2022</span> ⎸ <span style={{fontStyle: 'italic'}}>We love teachers!</span></p>
+<p id='Footer'>© Teach League <span style={{color: 'grey'}}>2022</span> ⎸ <span style={{fontStyle: 'italic'}}>We</span> ❤️ <span style={{fontStyle: 'italic'}}>teachers!</span></p>
     );
 }
 
--- teach-league/src/pages/TheTeachersLounge.js
+++ teach-league/src/pages/TheTeachersLounge.js
@@ -1,32 +1,28 @@
 import { Col, Row, Container } from 'reactstrap';
 import { Card, CardBody } from 'reactstrap';
-import SecretTeacher from '../features/SecretTeacher.js';
-import LoungeCard from '../components/LoungeCard';
 import LoungeCardList from '../utils/LoungeCardList';
-import { accordionData } from '../utils/teachersLoungeContent.js';
+import { loungeData } from '../utils/teachersLoungeContent.js';
 
 const TheTeachersLounge = () => {
     return (
         <Container>
-            <Card>
-           <CardBody>
-            <Row>
                 <Col>
         { <h1>The Teachers Lounge</h1> }
                 </Col>
-                <Col col col-md-8>
+             <Col id='lounge-subtitle' className="col-6 col-md-8">
         { <h2>Know that dream where you show up for the final and realize you forgot to go to class? Yeah we have that too but were supposed to be teaching it!</h2> }
                 </Col>
+            <Card>
+           <CardBody>
+            <Row>
         </Row>
         <Row>
-        <Col className="accordion col-12 col-lg-9 ">
-                    {accordionData.map(({ title, content, image }) => (
+        <Col className="card col-12 col-lg-9 ">
+                    {loungeData.map(({ title, content, image }) => (
                         <LoungeCardList title={title} content={content} image={image} />
                     ))}
                 </Col>
         </Row>
-        <LoungeCard />
-        <SecretTeacher />
         </CardBody>
         </Card>
         </Container>
