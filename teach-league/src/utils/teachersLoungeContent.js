8c171e0fd345630df561786f7ec8c08d2d53527e
Eitan Fire <eitanfire@gmail.com>
2022-10-05 10:12:06 -0600

    make rebuttal card

--- teach-league/src/utils/teachersLoungeContent.js
+++ teach-league/src/utils/teachersLoungeContent.js
@@ -6,17 +6,32 @@ import LoungeCard from './LoungeCard';
 import SecretTeacher from '../app/img/The-Secret-Teacher.webp';
 import '../App.css'
 import { CardImg, CardText } from 'reactstrap';
+import Rebuttal from '../app/img/moms-spaghetti.webp';
 
 export const loungeData = [
   {
-    // icon: <span className='icons'>📽</span>,
+    title: <span >The Rebuttal</span>,
+    image: <img className='lounge-card-image'
+    src={Rebuttal} alt='The Rebuttal' />,
+    content:
+      < >
+      What the teachers are saying. Teachers pushing back against the infantilization of the profession
+ <div className='btnStyle col-8'>
+          <a
+            role='button'
+            className='btn'
+            href='https://www.cpr.org/2022/03/22/denver-public-schools-teachers-burnout-mental-health-pandemic/' target="_blank" rel="noopener noreferrer">
+            Read More
+          </a>
+          </div>
+          </>
+  },
+  {
     title: <span >Secret Teacher</span>,
     image: <img
-    // className="classImage mx-auto d-none d-xxl-block" 
     src={SecretTeacher} alt='The Secret Teacher' />,
     content:
       < >
-      {/* <CardImg src="https://ichef.bbci.co.uk/images/ic/640xn/p08vy7s8.jpg"></CardImg> */}
                  The Secret Teacher is an anonymous blog from <span style={{fontStyle: 'italic'}}>The Guardian</span> where teachers can tell it like it is.
  <div className='btnStyle col-8'>
           <a
